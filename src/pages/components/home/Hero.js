cefd611c6711c70c2b03256bab5226926c56ef1d
Matt Kent <matt.kent@calderams.com>
2019-06-03 20:08:21 +0100

    fix horizontal overflow and particle density

--- src/pages/components/home/Hero.js
+++ src/pages/components/home/Hero.js
@@ -18,7 +18,52 @@ export default () => (
   <div className={compose(styles.wrapper)}>
     <Container>
       <Header />
-      <Particles style={{ position: 'absolute' }} />
+      <Particles
+        params={{
+          particles: {
+            number: {
+              value: 60,
+              density: {
+                enable: true,
+                value_area: 1500
+              }
+            },
+            line_linked: {
+              enable: true,
+              opacity: 0.7
+            },
+            move: {
+              direction: 'right',
+              speed: 0.5
+            },
+            size: {
+              value: 1
+            },
+            opacity: {
+              anim: {
+                enable: true,
+                speed: 1,
+                opacity_min: 0.5
+              }
+            }
+          },
+          interactivity: {
+            events: {
+              onclick: {
+                enable: true,
+                mode: 'push'
+              }
+            },
+            modes: {
+              push: {
+                particles_nb: 1
+              }
+            }
+          },
+          retina_detect: true
+        }}
+        style={{ position: 'absolute' }}
+      />
       <Intro />
       <Projects />
     </Container>
@@ -58,7 +103,6 @@ const styles = {
     color: 'white',
     padding: '2rem 0 4em',
     position: 'relative',
-    width: '100vw',
     overflow: 'hidden'
   },
   content: {
